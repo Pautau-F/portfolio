ebe4be41d0766c02815fbfc7100cf61e64795f67
Florian <caron.florian7620@gmail.com>
2022-02-17 15:35:51 +0100

    ADD languages DELETE / chars for subtitles

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,17 +5,32 @@ const Intro = () => {
     <div className="block intro">
       <h2 className="title box code">// Introduction </h2>
 
-        <div className="introBlock about box">
-            <h3 className="code">// About me</h3>
-            <p>Greetings! I'm Florian and I'm developping since 2019 so it's just the beginning but I aim at being better everyday.</p>
+      <div className="introBlock box">
+        <h3 className="code">About me</h3>
+        <p>
+          Greetings! I'm Florian and I'm developping since 2019 so it's just the
+          beginning but I aim at being better everyday.
+        </p>
+      </div>
+
+      <div className="introBlock box">
+        <h3 className="code">Current situation</h3>
+        <p>
+          I am currently in apprenticeship until September 20th 2023 therefore I
+          am not looking for a job at the moment.
+        </p>
+      </div>
+
+      <div className="introBlock languages box">
+        <h3 className="code">Comfortable with</h3>
+        <div className="languagesIcons">
+          <i class="fab fa-html5"/>
+          <i class="fab fa-css3-alt"/>
+          <i class="fab fa-js-square"/>
+          <i class="fab fa-react"/>
+          <i class="fab fa-php"/>
         </div>
-     
-      <div className="introBlock situation box">
-          <h3 className="code">// Current situation</h3>
-          <p>
-            I am currently in apprenticeship until September 20th 2023 therefore I am not looking for a job at the moment.
-          </p>
-        </div>
+      </div>
     </div>
   );
 };
